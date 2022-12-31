c125ee3d0d1f635b752c94e79fcfe8783969c7b9
SaiAshish <saiashish3760@gmail.com>
2022-12-31 15:38:42 +0530

    Add files via upload

---
+++ SpringPrometheus/src/test/java/com/SpringPrometheus/SpringPrometheus/SpringPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringPrometheus.SpringPrometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
